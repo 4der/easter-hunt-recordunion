ff9e4b6afed5544a0ef8cbf24febbfe00b70af2a
ader <adamsson.erika@gmail.com>
2018-03-26 09:08:56 +0200

    added test erika.html, 2.css, java.js

---
+++ java.js
@@ -0,0 +1,16 @@
+function toggleClass() {
+  this.classList.toggle("calendar-box-active")
+}
+
+document.getElementById("coverEffect0").onclick = toggleClass
+document.getElementById("coverEffect1").onclick = toggleClass
+document.getElementById("coverEffect2").onclick = toggleClass
+document.getElementById("coverEffect3").onclick = toggleClass
+document.getElementById("coverEffect4").onclick = toggleClass
+document.getElementById("coverEffect5").onclick = toggleClass
+document.getElementById("coverEffect6").onclick = toggleClass
+
+
+document.getElementById("coverEffect12").onclick = toggleClass
+document.getElementById("coverEffect13").onclick = toggleClass
+document.getElementById("coverEffect14").onclick = toggleClass
